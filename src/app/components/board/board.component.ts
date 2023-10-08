6637632b5145daba37421fc3ab5cb04083c2dbb2
Maciej Pliszek <pliszekmaciej0@gmail.com>
2023-10-08 23:36:43 +0200

    add: buttons disabled when winner != null

--- src/app/components/board/board.component.ts
+++ src/app/components/board/board.component.ts
@@ -27,7 +27,7 @@ export class BoardComponent {
   }
 
   makeMove(idx: number) {
-    if(!this.squares[idx]){
+    if(!this.squares[idx] && this.winner == null){
       this.squares.splice(idx, 1, this.currPlayer);
       this.xTurn = !this.xTurn;
     }
